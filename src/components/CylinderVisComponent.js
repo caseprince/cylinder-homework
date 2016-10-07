9d028d1d84807c02290aa1c6f46c195d83d23a98
caseprince <case.prince@ff0000.com>
2016-10-07 15:52:51 -0400

    Dynamic cylinder, controls, LESS

--- src/components/CylinderVisComponent.js
+++ src/components/CylinderVisComponent.js
@@ -4,43 +4,45 @@ import React from 'react';
 import React3 from 'react-three-renderer';
 import THREE from 'three';
 
-//import ReactNativeSlider from 'react-html5-slider';
+import ReactNativeSlider from 'react-html5-slider';
+import NumericInput from 'react-numeric-input';
 
-require('styles//CylinderVis.css');
+require('styles//CylinderVis.less');
 
 class CylinderVisComponent extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    // construct the position vector here, because if we use 'new' within render,
-    // React will think that things have changed when they have not.
-    this.cameraPosition = new THREE.Vector3(0, 0, 5);
-
     this.state = {
-      cubeRotation: new THREE.Euler()
+      cylinderRotation: new THREE.Euler(0.5, 0, 0),
+      cylinderRadius: 1,
+      cylinderHeight: 2,
+      directionalLightPosition: new THREE.Vector3(1, 0, 0),
+      scenePosition: new THREE.Vector3(0, 0, 0),
+      cameraPosition: new THREE.Vector3(0, 0, 5)
     };
 
-    this._onAnimate = () => {
+    /*this._onAnimate = () => {
       // we will get this callback every frame
 
-      // pretend cubeRotation is immutable.
+      // pretend cylinderRotation is immutable.
       // this helps with updates and pure rendering.
       // React will be sure that the rotation has now updated.
       this.setState({
-        cubeRotation: new THREE.Euler(
+        cylinderRotation: new THREE.Euler(
           0.5,
-          Math.PI/2,//this.state.cubeRotation.y + 0.00,
+          this.state.cylinderRotation.y + 0.01,
           0
-        ),
-        directionalLightPosition: new THREE.Vector3(1, 0, 0),
-        scenePosition: new THREE.Vector3(0, 0, 0)
+        )
       });
-    };
+    };*/
   }
 
   render() {
-    const width = 500; // canvas width
-    const height = 500; // canvas height
+    const width = 500, // canvas width
+          height = 500, // canvas height
+          cylinderHeightMax = 3,
+          cylinderRadiusMax = 2;
 
     return (
       <div className='cylindervis-component'>
@@ -48,7 +50,7 @@ class CylinderVisComponent extends React.Component {
           mainCamera="camera" // this points to the perspectiveCamera which has the name set to "camera" below
           width={width}
           height={height}
-          onAnimate={this._onAnimate}
+          //onAnimate={this._onAnimate}
         >
           <scene>
             <perspectiveCamera
@@ -57,8 +59,7 @@ class CylinderVisComponent extends React.Component {
               aspect={width / height}
               near={0.9}
               far={1000}
-
-              position={this.cameraPosition}
+              position={this.state.cameraPosition}
             />
             <ambientLight
               color={0x404040}
@@ -68,13 +69,11 @@ class CylinderVisComponent extends React.Component {
               position={this.state.directionalLightPosition}
               lookAt={this.state.scenePosition}
             />
-            <mesh
-              rotation={this.state.cubeRotation}
-            >
+            <mesh rotation={this.state.cylinderRotation}>
               <cylinderGeometry
-                radiusTop={1}
-                radiusBottom={1}
-                height={2}
+                radiusTop={this.state.cylinderRadius}
+                radiusBottom={this.state.cylinderRadius}
+                height={this.state.cylinderHeight}
                 radialSegments={100}
               />
               <meshPhongMaterial
@@ -87,13 +86,55 @@ class CylinderVisComponent extends React.Component {
             </mesh>
           </scene>
         </React3>
-        {/*<ReactNativeSlider
-          value={this.state.currentValue}
-          handleChange={this.changeValue}
-          step={this.state.step}
-          max={this.state.max}
-          min={this.state.min}
-          disabled='disabled' />*/}
+
+        <div className="controls">
+          <div>
+            <label htmlFor="cylinder-radius">Radius</label>
+            <ReactNativeSlider
+              value={this.state.cylinderRadius}
+              handleChange={e => {
+                this.setState({ cylinderRadius: Number(e.target.value) });
+              }}
+              step={0.01}
+              max={cylinderRadiusMax}
+              min={0}
+              id="cylinder-radius"
+              />
+            <NumericInput
+              value={this.state.cylinderRadius}
+              onChange={valueAsNumber => {
+                this.setState({ cylinderRadius: valueAsNumber });
+              }}
+              step={0.1}
+              max={cylinderRadiusMax}
+              min={0}
+              precision={2} 
+              />
+          </div>
+
+          <div>
+            <label htmlFor="cylinder-radius">Height</label>
+            <ReactNativeSlider
+              value={this.state.cylinderHeight}
+              handleChange={e => {
+                this.setState({ cylinderHeight: Number(e.target.value) });
+              }}
+              step={0.01}
+              max={cylinderHeightMax}
+              min={0}
+              />
+            <NumericInput
+              value={this.state.cylinderHeight}
+              onChange={valueAsNumber => {
+                this.setState({ cylinderHeight: valueAsNumber });
+              }}
+              step={0.1}
+              max={cylinderHeightMax}
+              min={0}
+              precision={2} 
+              />
+          </div>
+        </div>
       </div>
     );
   }
